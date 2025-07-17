70ad158fad64833110fc26626c26a7781bcdaae3
vietphan565 <phantuanviet0706@gmail.com>
2025-07-17 10:35:10 +0700

    First update configure project

---
+++ src/main/java/com/example/pancharm/PancharmApplication.java
@@ -0,0 +1,13 @@
+package com.example.pancharm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PancharmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PancharmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pancharm/PancharmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pancharm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PancharmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
