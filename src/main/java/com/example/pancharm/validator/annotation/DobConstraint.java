ef3417420e8d567b1f478a452f11f7b0a7ece493
vietphan565 <phantuanviet0706@gmail.com>
2025-07-21 21:37:26 +0700

    Update custom validation example

---
+++ src/main/java/com/example/pancharm/validator/annotation/DobConstraint.java
@@ -0,0 +1,24 @@
+package com.example.pancharm.validator.annotation;
+
+import com.example.pancharm.validator.implementation.DobValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = DobValidator.class)
+public @interface DobConstraint {
+	String message() default "Invalid date of birth";
+
+	int min();
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
