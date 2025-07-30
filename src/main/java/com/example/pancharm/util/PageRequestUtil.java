752860d5f3715e61b584d65b408f06361f24ffad
vietphan565 <phantuanviet0706@gmail.com>
2025-07-30 14:20:07 +0700

    Update reuse function comment

--- src/main/java/com/example/pancharm/util/PageRequestUtil.java
+++ src/main/java/com/example/pancharm/util/PageRequestUtil.java
@@ -7,6 +7,11 @@ import org.springframework.data.domain.Sort;
 import com.example.pancharm.dto.request.base.PageDefaultRequest;
 
 public class PageRequestUtil {
+    /**
+     * @desc This function is reuse for pagination & sort direction of Objects
+     * @param request
+     * @return
+     */
     public static Pageable from(PageDefaultRequest request) {
         Sort.Direction direction =
                 "asc".equalsIgnoreCase(request.getSortDirection()) ? Sort.Direction.ASC : Sort.Direction.DESC;
