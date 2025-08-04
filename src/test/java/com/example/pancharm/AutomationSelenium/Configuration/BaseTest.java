57dcacf174508edd9a0d288fd2264ce9c57a766c
Nam <doublelift2012@gmail.com>
2025-08-04 21:54:27 +0700

    add automation selenium

---
+++ src/test/java/com/example/pancharm/AutomationSelenium/Configuration/BaseTest.java
@@ -0,0 +1,4 @@
+package com.example.pancharm.AutomationSelenium.Configuration;
+
+public class BaseTest {
+}
---
+++ src/test/java/com/example/pancharm/service/UserServiceTest.java
@@ -0,0 +1,4 @@
+package com.example.pancharm.service;
+
+public class UserServiceTest {
+}
